3b181cf5907a1a41e7bb11683abd749d59cf9daf
Lauren Grillz <lauren.grills@gmail.com>
2020-02-27 14:48:21 -0600

    imported data to main app

---
+++ src/components/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    <h1 class="title">Bardic Words</h1>
+    <div class="container">
+      <button class="button is-primary">Inspire!</button>
+      <p>Inspire your comrades or yourself with Bardic Inspiration!</p>
+      <button class="button is-danger">Mock!</button>
+      <p>Bring your enemies down a size with Vicious Mockery!</p>
+    </div>
+    <ul>
+      <li v-for="insult in insults" :key="insult.id">
+        {{ insult.description }}
+      </li>
+      <li v-for="inspire in inspires" :key="inspire.id">
+        {{ inspire.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import inspos from "../shared/inspos.json";
+import mockeries from "../shared/mockeries.json";
+
+export default {
+  name: "Main",
+  data() {
+    return {
+      insults: mockeries,
+      inspires: inspos
+    };
+  }
+};
+</script>
